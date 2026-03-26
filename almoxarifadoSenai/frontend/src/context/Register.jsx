17ca1502cd38d4bdc054493bb519100c41eff9ab
Helen Yohanna Ventura Della Roveri <86626344+HelenDellaRoveri@users.noreply.github.com>
2026-03-26 17:08:05 -0300

    Add files via upload

---
+++ almoxarifadoSenai/frontend/src/context/Register.jsx
@@ -0,0 +1,27 @@
+import { Container, Box, TextField, Button, Typography, Paper, MenuItem } from "@mui/material";
+import { useState } from "react";
+import { api } from "../services/Api";
+import { useNavigate } from "react-router-dom"
+
+export default function Register(){
+    const navigate = useNavigate();
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [setor, setSetor] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [error, setError] = useState("");
+
+    const handleRegister = async () => {
+        if(!name || !email || !setor || !password || !confirmPassword){
+            setError("Preencha todos os campos!");
+            return;
+        }
+
+        if(password !== confirmPassword){
+            setError("As senhas não condicidem!");
+            return;
+        }
+    }
+
+}
